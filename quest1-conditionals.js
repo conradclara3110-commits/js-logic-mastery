b225bfe9ad8f0f1cf97018ebe073060fd5e926a1
Clara Millinga <conradclara3110@gmail.com>
2026-03-22 22:12:03 +0300

    Add greet, add, and multiply functions

---
+++ quest1-conditionals.js
@@ -0,0 +1,20 @@
+function greet(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+greet('Alex');     
+greet('Jordan');  
+
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(5, 3);
+console.log(result);  
+
+function multiply(x, y) {
+  return x * y;
+}
+
+const product = multiply(4, 6);
+console.log(product); 
