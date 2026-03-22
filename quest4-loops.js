0ab9f4143ad78b2a5d5e351e238bba7877dd4bfa
Clara Millinga <conradclara3110@gmail.com>
2026-03-22 22:23:41 +0300

    Add fruit and number array manipulations

---
+++ quest4-loops.js
@@ -0,0 +1,22 @@
+const fruits = ["apple", "banana", "orange"];
+
+console.log(fruits[0]); 
+
+fruits.push("grape");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits); 
+
+console.log(fruits.length); 
+console.log(fruits.includes("banana")); 
+
+const numbers = [10, 20, 30, 40, 50];
+
+numbers.push(60);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+console.log(numbers.length);
